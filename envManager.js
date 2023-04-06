70d0b23332f3d5f29c706d3d26ad719961b99d49
Yakali, Yagizhan Necat (ADV D EU TR AP&I PIA1) <yagizhan.yakali@siemens.com>
2023-04-06 13:11:31 +0300

    update envManager script

--- envManager.js
+++ envManager.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
+const path = require("path");
 
-const dir = "src/environment";
+const dir = "src/environments";
 const file = "environment.ts";
 const prodFile = "environment.prod.ts";
 
@@ -11,16 +12,18 @@ fs.access(dir, fs.constants.F_OK, (err) => {
     console.log("src does not exist, creating now...", process.cwd());
     fs.mkdir(dir, { recursive: true }, (err) => {
       if (err) throw err;
+      console.log("Folder created successfully " + dir, process.cwd());
     });
   }
 
   try {
-    fs.writeFileSync(`${dir}/${file}`, content);
-    fs.writeFileSync(`${dir}/${prodFile}`, content);
+    fs.writeFileSync(path.join(dir, file), content);
+    fs.writeFileSync(path.join(dir, prodFile), content);
 
-    if (fs.existsSync(`${dir}/${file}`)) {
+    if (fs.existsSync(path.join(dir, file))) {
       console.log("environment.ts created successfully", process.cwd());
-      const str = fs.readFileSync(`${dir}/${file}`, "utf8").toString();
+      const str = fs.readFileSync(path.join(dir, file), "utf8").toString();
+      console.log(str);
     }
   } catch (err) {
     console.log(err);
