54224c34948b7fb84ba0d0104e5c7ff74595bbd8
Mehul <mehulshrivastava1011@gmail.com>
2023-04-24 22:23:30 +0530

    Issue: #11 Changes for inclusion of flowbite, used cnd links for making interactive functionality to work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("flowbite/plugin")],
 };
